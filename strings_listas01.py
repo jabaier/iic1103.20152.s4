11d55f1d736de2e07676285ab7c7f09f6542bb5a
Jorge Baier <jabaier@gmail.com>
2015-10-01 09:50:30 -0300

    encriptacion rot y suma de listas

--- strings_listas01.py
+++ strings_listas01.py
@@ -13,8 +13,44 @@ def sumalista_cool(l):
         suma = suma + elemento
     return suma
 
+def desafio_google(l):
+    i = 0
+    while i < len(l):
+        if sumalista(l[:i]) == sumalista(l[i+1:]):
+            return i
+        i = i + 1
+    return -1
 
-v=sumalista_cool([3,4,5])
+def mayusculas():
+    i = ord('A')
+    fin = ord('Z')
+    mayusculas = ''
+    while i <= fin:
+        mayusculas = mayusculas + chr(i)
+        i = i + 1
+    return mayusculas
 
-print(sumalista_cool([3,4,5]))
-print(sumalista_cool([1,1,1,1]))
+def minusculas():
+    return mayusculas().lower()
+
+
+def rot_encriptacion(mensaje, incremento):
+    M = mayusculas()
+    m = minusculas()
+    mensaje_encriptado=''
+    for c in mensaje:
+        indiceM = M.find(c)
+        indicem = m.find(c)
+        if indiceM >= 0:         # es mayuscula
+            caracter = M[(indiceM+incremento)%26]
+        elif indicem >= 0:
+            caracter = m[(indicem+incremento)%26]
+        else:
+            caracter = c
+        mensaje_encriptado = mensaje_encriptado + caracter
+    return mensaje_encriptado
+
+enc = rot_encriptacion("Este es un Mensaje muy secreto",13)
+desenc = rot_encriptacion(enc,13)
+print(enc)
+print(desenc)
